96255e964a1a9bd566e5dced8b5bb1ab86cf912a
Phill <14913130+phillram@users.noreply.github.com>
2019-12-27 10:05:05 -0800

    Added samples to all current datacenters and RDC

--- OSX_Sauce_Connect.bash
+++ OSX_Sauce_Connect.bash
@@ -2,49 +2,74 @@
 
 ulimit -n 8000
 
-/Users/phillipramirez/sauce-connect/4.5.3/bin/sc \
+~/Sauce\ Connect/4.5.4/bin/sc \
     --user $SAUCE_USERNAME \
     --api-key $SAUCE_ACCESS_KEY \
-    --no-remove-colliding-tunnels \
-    --tunnel-identifier="Phill Tunnel One" \
+    --tunnel-identifier="US VDC Latest" \
     --se-port 0 \
-    --pidfile /Users/phillipramirez/sauce-connect/pid/tunnel1.pid \
-    --logfile /Users/phillipramirez/sauce-connect/logs/tunnel1.log &
+    --pidfile ~/Sauce\ Connect/pid/us_vdc_latest.pid \
+    --logfile ~/Sauce\ Connect/logs/us_vdc_latest.log &
 
-/Users/phillipramirez/sauce-connect/4.5.3/bin/sc \
+~/Sauce\ Connect/4.5.3/bin/sc \
     --user $SAUCE_USERNAME \
     --api-key $SAUCE_ACCESS_KEY \
-    --no-remove-colliding-tunnels \
-    --tunnel-identifier="Phill Tunnel Two" \
+    --tunnel-identifier="US VDC Prior" \
     --se-port 0 \
-    --pidfile /Users/phillipramirez/sauce-connect/pid/tunnel2.pid \
-    --logfile /Users/phillipramirez/sauce-connect/logs/tunnel2.log &
+    --pidfile ~/Sauce\ Connect/pid/us_vdc_one_prior.pid \
+    --logfile ~/Sauce\ Connect/logs/us_vdc_one_prior.log &
 
-/Users/phillipramirez/sauce-connect/4.5.3/bin/sc \
+~/Sauce\ Connect/4.5.4/bin/sc \
     --user $SAUCE_USERNAME \
     --api-key $SAUCE_ACCESS_KEY \
-    --no-remove-colliding-tunnels \
-    --tunnel-identifier="Phill Tunnel Three" \
+    --tunnel-identifier="EU VDC Latest" \
+    --rest-url https://eu-central-1.saucelabs.com/rest/v1 \
     --se-port 0 \
-    --pidfile /Users/phillipramirez/sauce-connect/pid/tunnel3.pid \
-    --logfile /Users/phillipramirez/sauce-connect/logs/tunnel3.log &
+    --pidfile ~/Sauce\ Connect/pid/eu_vdc_latest.pid \
+    --logfile ~/Sauce\ Connect/logs/eu_vdc_latest.log &
 
-/Users/phillipramirez/sauce-connect/4.5.2/bin/sc \
+~/Sauce\ Connect/4.5.3/bin/sc \
     --user $SAUCE_USERNAME \
     --api-key $SAUCE_ACCESS_KEY \
-    --no-remove-colliding-tunnels \
-    --tunnel-identifier="Phill Tunnel Four" \
+    --tunnel-identifier="EU VDC Prior" \
+    --rest-url https://eu-central-1.saucelabs.com/rest/v1 \
     --se-port 0 \
-    --pidfile /Users/phillipramirez/sauce-connect/pid/tunnel4.pid \
-    --logfile /Users/phillipramirez/sauce-connect/logs/tunnel4.log &
+    --pidfile ~/Sauce\ Connect/pid/eu_vdc_one_prior.pid \
+    --logfile ~/Sauce\ Connect/logs/eu_vdc_one_prior.log &
 
-/Users/phillipramirez/sauce-connect/4.5.2/bin/sc \
+~/Sauce\ Connect/4.5.4/bin/sc \
     --user $SAUCE_USERNAME \
-    --api-key $SAUCE_ACCESS_KEY \
-    --no-remove-colliding-tunnels \
-    --tunnel-identifier="Phill Tunnel Five" \
+    --api-key $SAUCE_RDC_APIKEY \
+    --tunnel-identifier="US RDC Latest" \
+    --rest-url https://us1.api.testobject.com/sc/rest/v1 \
     --se-port 0 \
-    --pidfile /Users/phillipramirez/sauce-connect/pid/tunnel5.pid \
-    --logfile /Users/phillipramirez/sauce-connect/logs/tunnel5.log &
+    --pidfile ~/Sauce\ Connect/pid/us_rdc_latest.pid \
+    --logfile ~/Sauce\ Connect/logs/us_rdc_latest.log &
+
+~/Sauce\ Connect/4.5.3/bin/sc \
+    --user $SAUCE_USERNAME \
+    --api-key $SAUCE_RDC_APIKEY \
+    --tunnel-identifier="US RDC Prior" \
+    --rest-url https://us1.api.testobject.com/sc/rest/v1 \
+    --se-port 0 \
+    --pidfile ~/Sauce\ Connect/pid/us_rdc_one_prior.pid \
+    --logfile ~/Sauce\ Connect/logs/us_rdc_one_prior.log &
+
+~/Sauce\ Connect/4.5.4/bin/sc \
+    --user $SAUCE_USERNAME \
+    --api-key $SAUCE_RDC_APIKEY \
+    --tunnel-identifier="EU RDC Latest" \
+    --rest-url https://eu1.api.testobject.com/sc/rest/v1 \
+    --se-port 0 \
+    --pidfile ~/Sauce\ Connect/pid/eu_rdc_latest.pid \
+    --logfile ~/Sauce\ Connect/logs/eu_rdc_latest.log &
+
+~/Sauce\ Connect/4.5.3/bin/sc \
+    --user $SAUCE_USERNAME \
+    --api-key $SAUCE_RDC_APIKEY \
+    --tunnel-identifier="EU VDC Prior" \
+    --rest-url https://eu1.api.testobject.com/sc/rest/v1 \
+    --se-port 0 \
+    --pidfile ~/Sauce\ Connect/pid/eu_rdc_one_prior.pid \
+    --logfile ~/Sauce\ Connect/logs/eu_rdc_one_prior.log &
 
 wait
